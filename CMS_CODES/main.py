29120ae618688476153f34e759c22bc8b36f8f83
Aryan <arian.ashrafi.personal@outlook.com>
2023-09-20 23:57:00 +0430

    Main file

--- CMS_CODES/main.py
+++ CMS_CODES/main.py
@@ -4,3 +4,19 @@ from flask_bcrypt import Bcrypt
 from forms import RegisterPatientForm, PatientLoginForm, DoctorLoginForm, AdminLoginForm, RegisterDoctorForm, ConfirmAppointmentForm
 from flask_login import LoginManager, login_user, UserMixin, logout_user, current_user
 from flask_mail import Mail, Message
+
+
+app = Flask(__name__)
+app.app_context().push()
+app.config['SECRET_KEY'] = '09144228463Fa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+mail = Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'clinic.tabrizuni@gmail.com'
+app.config['MAIL_PASSWORD'] = 'bpjonlmufrsaqsef'
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
